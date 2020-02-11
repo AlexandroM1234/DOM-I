5c2b3df06355f46e35b52526e93ca8fe85273552
Alex Martinez <alexmartinez0071234@gmail.com>
2020-02-10 21:28:32 -0500

    fixed everything

--- js/index.js
+++ js/index.js
@@ -41,19 +41,24 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+// selects nav
 let navMain= document.querySelector('nav');
 
+// changes nav background-color
 navMain.style.backgroundColor = 'green';
 
+// collects all anchors on the page
 let navItems = document.querySelectorAll('a')
 
-navItems[0].textContent='Services'
-navItems[1].textContent='Product'
-navItems[2].textContent='Vision'
-navItems[3].textContent='Features'
-navItems[4].textContent='About'
-navItems[5].textContent='Contact'
+// changes anchor title depending on the index number
+navItems[0].textContent=siteContent["nav"]["nav-item-1"]
+navItems[1].textContent=siteContent["nav"]["nav-item-2"]
+navItems[2].textContent=siteContent["nav"]["nav-item-3"]
+navItems[3].textContent=siteContent['nav']["nav-item-4"]
+navItems[4].textContent=siteContent["nav"]["nav-item-5"]
+navItems[5].textContent=siteContent["nav"]["nav-item-6"]
 
+// creates 2 new anchors
 const newAnchor= document.createElement('a');
 newAnchor.textContent='Meet the Team';
 navMain.appendChild(newAnchor)
@@ -62,49 +67,70 @@ const nuevoAnchor= document.createElement('a');
 nuevoAnchor.textContent='Random Link';
 navMain.prepend(nuevoAnchor);
 
+
+// selects the one h1 on the page
 let title = document.querySelector('h1')
 
-title.textContent='Dom is Awesome';
+// changes what the h1 says
+title.textContent=siteContent["cta"]["h1"];
 
+// selects button
 const button = document.querySelector('button');
 
-button.textContent='Get Started';
+//chagnges the button content
+button.textContent=siteContent["cta"]["button"];
 
+// selects the img
 const ctaImg = document.getElementById('cta-img')
 
-ctaImg.src= 'img/header-img.png';
+// changes the img src
+ctaImg.setAttribute('src',siteContent["cta"]["img-src"]);
 
+//selects all h4 on the page
 const subTitles = document.querySelectorAll('h4')
 
-subTitles[0].textContent='Features';
-subTitles[1].textContent='About';
-subTitles[2].textContent='Services';
-subTitles[3].textContent='Product';
-subTitles[4].textContent='Vision';
+// changes what each h4 says depending on the index
+subTitles[0].textContent=siteContent["main-content"]["features-h4"];
 
+subTitles[1].textContent=siteContent["main-content"]['about-h4'];
+
+subTitles[2].textContent=siteContent['main-content']['services-h4'];
+
+subTitles[3].textContent=siteContent['main-content']['product-h4'];
+
+subTitles[4].textContent=siteContent['main-content']['vision-h4'];
+
+subTitles[5].textContent=siteContent['contact']['contact-h4'];
+
+
+// selects the middle img
 const stretchImg = document.getElementById('middle-img')
 
-stretchImg.src='img/mid-page-accent.jpg'
+// changes the img src
+stretchImg.setAttribute('src',siteContent["main-content"]["middle-img-src"])
 
+// selects p tags on the page
 const pageText = document.querySelectorAll('p');
 
-pageText[0].textContent='Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis';
+
+// p tags of the main content
+pageText[0].textContent=siteContent["main-content"]["features-content"];
+
+pageText[1].textContent=siteContent["main-content"]["about-content"];
+
+pageText[2].textContent=siteContent['main-content']['services-content'];
+
+pageText[3].textContent=siteContent['main-content']['product-content'];
+
+pageText[4].textContent=siteContent['main-content']['vision-content']
 
 
-pageText[1].textContent='About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+// p tags of the contact
+pageText[5].textContent=siteContent['contact']['address']
 
+pageText[6].textContent=siteContent['contact']['phone']
 
-pageText[2].textContent='Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+pageText[7].textContent=siteContent['contact']['email']
 
-
-pageText[3].textContent='Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-
-
-pageText[4].textContent='Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
-
-subTitles[5].textContent='About';
-
-pageText[5].textContent='123 Way 456 Street Somewhere USA';
-pageText[6].textContent='1(888)888-888';
-pageText[7].textContent='sales@greatidea.io';
-pageText[8].textContent="Copyright Great Idea! 2020";
+// p tag of the footer
+pageText[8].textContent=siteContent['footer']['copyright']
